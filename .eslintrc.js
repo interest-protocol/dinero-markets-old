48b8e2da2a0038a2066801baba47ae975729446f
josemvcerqueira <jose@ilovemochi.com>
2022-03-03 19:14:11 +0000

    ci: update work flow for pull requests

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,7 @@ module.exports = {
     'unicorn/escape-case': 'error',
     'unicorn/expiring-todo-comments': 'error',
     'unicorn/explicit-length-check': 'error',
-    'unicorn/filename-case': 'error',
+    'unicorn/filename-case': 'off',
     'unicorn/import-index': 'off',
     'unicorn/import-style': 'error',
     'unicorn/new-for-builtins': 'error',
